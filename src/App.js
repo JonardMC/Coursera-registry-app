9f4a7e8df846d79aadaf49d2fb467c018b555f77
Jonard Carganilla <jonard.m.carganilla@lpstech.com>
2024-02-27 20:00:01 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {BrowserRouter , Switch, Route, Link} from "react-router-dom"
+import Home from "./routes/Home"
+import Registry from "./routes/Registry"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    //const [name, setName] = useState("")
+
+    return(
+        <div className="App">
+          <BrowserRouter>
+          <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/registry">
+                <Registry />
+              </Route>
+          </Switch>
+          </BrowserRouter>
+            {/* <button onClick={() => setName("hello")} > Set Name</button>
+        <MyComponent name = {name}/> */}
+        </div>
+    )
 }
 
+// function MyComponent(props){
+//     return <h1>{props.name}</h1>
+// }
+
 export default App;
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
